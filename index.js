3215c28d076867f84862dcfb7e9a3c70d1ee5e11
Zain Ali <zain08816@gmail.com>
2020-04-14 17:58:41 -0600

    add working A/B testing style

--- index.js
+++ index.js
@@ -6,7 +6,26 @@ addEventListener('fetch', event => {
  * @param {Request} request
  */
 async function handleRequest(request) {
-  return new Response('Hello worker!', {
-    headers: { 'content-type': 'text/plain' },
+
+  let url = 'https://cfw-takehome.developers.workers.dev/api/variants'
+  let response = await fetch(url)
+  let sites = await response.json();
+  let site_text = ""
+
+  let random = Math.random()
+  if (random < 0.5) {
+
+    let site_response = await fetch(sites.variants[0]);
+    site_text = await site_response.text();
+
+  } else {
+
+    let site_response = await fetch(sites.variants[1]);
+    site_text = await site_response.text();
+
+  }
+
+  return new Response(site_text, {
+    headers: { 'content-type': 'text/html' },
   })
 }
